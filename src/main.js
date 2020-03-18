98ee9c2dad75478a55c1dc6a7fe1ae708dedd04d
sashaadazhii <sashki.sashka@gmail.com>
2020-03-18 09:50:16 +0200

    with history

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import App from './App.vue'
+import Paginate from 'vuejs-paginate'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
@@ -23,6 +24,7 @@ Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
 
 
 firebase.initializeApp({
---
+++ src/mixins/pagination.mixin.js
@@ -0,0 +1,27 @@
+import _ from 'lodash'
+
+export default {
+  data() {
+    return {
+      page: +this.$route.query.page || 1,
+      pageSise: 4,
+      pageCount: 0,
+      allItems: [],
+      items: [],
+    }
+  },
+
+  methods: {
+    pageChangeHandler(page) {
+      this.$router.push(`${this.$route.path}?page=${page}`)
+      this.items = this.allItems[page - 1] || this.allItems[0]
+    },
+
+
+    setupPagination(allItems) {
+      this.allItems = _.chunk(allItems, this.pageSise)
+      this.pageCount = _.size(this.allItems)
+      this.items = this.allItems[this.page - 1] || this.allItems[0]
+    }
+  }
+}
